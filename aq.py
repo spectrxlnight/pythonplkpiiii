e090ba239834c499890d3e9834cba22db836799d
lisitskynikita <lisitskynikita@gmail.com>
2025-09-06 10:33:15 +0300

    Add files via upload

--- aq.py
+++ aq.py
@@ -1,2 +1,22 @@
-for i in range(1, 1000, 15):
-    print(i)
+import math
+def y(x):
+    if x < 2:
+        return False
+    if x == 2:
+        return True
+    if x % 2 == 0:
+        return False
+    r = int(math.sqrt(x))
+    for d in range(3, r + 1, 2):
+        if x % d == 0:
+            return False
+    return True
+
+n = int(input("Введіть n: "))
+primes = []
+for i in range(2, n + 1):
+    if y(i):
+        primes.append(i)
+
+print("Прості числа від 1 до", n, ":")
+print(primes)
